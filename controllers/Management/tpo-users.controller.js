45f5ae34e3c0b44545607e5b0ae899c9eba02f63
Tejas <157827810+tejaswillcode@users.noreply.github.com>
2026-01-17 18:35:19 +0530

    Delete controllers directory

--- controllers/Management/tpo-users.controller.js
+++
@@ -1,8 +0,0 @@
-const Users = require('../../models/user.model');
-
-const UsersTPO = async (req, res) => {
-  const tpoUsers = await Users.find({ role: "tpo_admin" });
-  return res.json({ tpoUsers })
-}
-
-module.exports = UsersTPO;
